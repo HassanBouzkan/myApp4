2c8823a3c01065ec5b8f68eb8a884b03c3b0abd2
Hassan BOUZKAN <123655399+HassanBouzkan@users.noreply.github.com>
2023-01-30 16:31:19 +0100

    Update sample.java

--- sample.java
+++ sample.java
@@ -3,7 +3,7 @@ public class sample {
 
     System.out.printf("Hello, there. ");
     System.out.printf("My name is Hassan. ");
-    System.out.printf("I'm 20 years old");
+    System.out.printf("I'm 20 years old. ");
     System.out.printf("Nice to meet you guys. ");
     System.out.println("I hope we'll have a good time together.");
     System.out.printf("with all my due respect\nHassan ");
