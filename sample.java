13807b5d7b063153c709c9d3a35d07295157f028
Hassan Bouzkan <bouzkanhassan@gmail.com>
2023-01-30 15:51:38 +0100

    create sample.java

---
+++ sample.java
@@ -0,0 +1,8 @@
+public class sample {
+    public static void main (String[] args){
+
+    System.out.printf("Hello, there. ");
+    System.out.printf("My name is Hassan. ");
+    System.out.printf("Nice to meet you guys.");
+    }
+}
